391e14c3cea7cbb21f85919439190caeb87ee01e
Hector-Junior <nuagecloud43@gmail.com>
2025-11-14 17:17:50 +0100

    add PixelBlast

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,30 @@
-export default function Home(){
+import PixelBlast from '../components/PixelBlast'
+
+export default function Home() {
     return (
-        <>
-        <h1>Home</h1>
-        </>
+        <div style={{ minHeight: '100vh', position: 'relative', zIndex: 1 }}>
+            <PixelBlast
+                style={{
+                    position: 'fixed',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '50vh',
+                    zIndex: 0
+                }}
+                variant="square"
+                pixelSize={6}
+                color="#B19EEF"
+                patternScale={3}
+                patternDensity={1.2}
+                pixelSizeJitter={0.5}
+                rippleSpeed={0.4}
+                rippleThickness={0.12}
+                rippleIntensityScale={1.5}
+                speed={0.6}
+                edgeFade={0.25}
+                transparent
+            />
+        </div>
     )
 }
