75a23d552455b7d3b7e037da9085840609079efa
Hector-Junior <nuagecloud43@gmail.com>
2025-11-14 17:18:08 +0100

    add PixelBlast

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from './pages/Home'
 import Work from './pages/Work'
 import About from './pages/About'
 
+import './App.css'
+
 import Header from './components/Header'
 
 export default function App(){
