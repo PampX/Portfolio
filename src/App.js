8371517d41987e74369f02595596b4b46cf49f88
PampX <nuagecloud43@gmail.com>
2025-12-12 21:22:57 +0100

    un footer qui foot

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './pages/About'
 import './App.css' 
 
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
           <Route path='/work' element={<Work />} />
           <Route path='/about' element={<About />} />
         </Routes>
+        <Footer/>
       </div>
     </Router>
   )
---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import "../styles/Footer.css";
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <div className="footer-left">
+                    <p className="footer-name">Timothée Baudequin</p>
+                    <p className="footer-desc">
+                        Developer • IA • Web • Game Dev
+                    </p>
+                </div>
+
+                <div className="footer-center">
+                    <a href="mailto:timotheebaudequin@gmail.com">Email</a>
+                    <a href="https://github.com/PampX" target="_blank">GitHub</a>
+                    <a href="https://www.linkedin.com/in/timothee-baudequin/" target="_blank">LinkedIn</a>
+                    <a href="https://www.instagram.com/hec.torjunior" target="_blank">Instagram</a>
+                    <a href="https://www.instagram.com/hec.torjunior3D" target="_blank">Instagram3D</a>
+                
+                </div>
+
+                <div className="footer-right">
+                    <p>© {new Date().getFullYear()}</p>
+                </div>
+            </div>
+        </footer>
+    );
+}
